51c27acc25abda64f739f861343a3ab21953631b
shunta071 <2095071@gmail.com>
2017-08-04 10:03:49 +0000

    ランキングの実装2

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
 'use strict';
-const fs = require('fs');
-const  readline = require('readline');
+const fs = require('fs'); // File Systemモジュールの取り出し
+const readline = require('readline'); // ファイルを1行ずつ読み込むモジュールの取り出し
 const rs = fs.ReadStream('./popu-pref.csv');
-const rl = readline.createInterface({ 'input': rs, 'output': {} });
-const map = new Map(); // key : 都道府県　value :  集計データのオブジェクト
+const rl = readline.createInterface({'input': rs, 'output' : {} });
+const map = new Map(); //key: 都道府県 value: 集計データのオブジェクト
 rl.on('line', (lineString) => {
-    const columns = lineString.split(',');
-    const year = parseInt(columns[0]);
+    const columns = lineString.split(',');　// lineString.sprit()　は読み込んだ文字列を分割
+    const year = parseInt(columns[0]); //parseInt は文字列を整数値に直す
     const prefecture = columns[2];
-    const popu = parseInt(columns[7]);
-    if (year === 2010 || year ===2015) {
-        let value = map.get(prefecture);
-        if(!value) {
-            value = {
-                p10: 0,
-                p15: 0,
-                change: null
-            };
-        }
-        if (year === 2010) {
-            value.p10 += popu;
-        }
-        if (year === 2015) {
-            value.p15 += popu;
-        }
-        map.set(prefecture, value);
+    const popu = parseInt(columns[7]);　//parseInt は文字列を整数値に直す
+    if (year === 2010 || year === 2015) {
+       let value = map.get(prefecture);
+        if (!value) {
+         value = {
+             p10: 0,
+             p15: 0,
+            change: null
+         };
+       }
+    if (year === 2010){
+        value.p10 += popu;
     }
+    if (year === 2015){
+        value.p15 += popu;
+    }
+    map.set(prefecture, value);
+}
+
 });
+
 rl.resume();
 rl.on('close', () => {
     for (let pair of map) {
@@ -35,9 +37,10 @@ rl.on('close', () => {
     }
     const rankingArray = Array.from(map).sort((p1, p2) => {
         return p2[1].change - p1[1].change;
-   });
-   const rankingStrings = rankingArray.map((p) => {
-       return p[0] + ': ' + p[1].p10 + '=>' + p[1].p15 + ' 変化率:' + p[1].change;
-   });
+    });
+    const rankingStrings = rankingArray.map((p) => {
+        return p[0] + ': ' + p[1].p10 + '=>' + p[1].p15 + ' 変化率:' + p[1].change;
+    });
+
     console.log(rankingStrings);
 });
